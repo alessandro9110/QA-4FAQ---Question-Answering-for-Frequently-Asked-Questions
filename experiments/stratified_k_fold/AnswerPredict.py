7c67f19ede1f2fa4942af51d5c70847176cbcb18
Alessandro <a.armillotta91@gmail.com>
2018-03-16 00:43:30 +0100

    experiments

---
+++ experiments/stratified_k_fold/AnswerPredict.py
@@ -0,0 +1,191 @@
+import os
+import sys
+import certifi
+import unicodedata
+import pandas as pd
+import numpy as np
+import re
+import csv
+import math
+import requests
+import time
+import random
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import gensim
+import keras
+import h5py
+import pydot
+import os
+import sklearn
+import operator
+import time
+
+from keras.layers.merge import Add
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical, plot_model
+from keras.layers import Conv2D, MaxPooling2D, Embedding, Merge, LSTM, Bidirectional, GlobalMaxPooling1D, Dot, Lambda, Dropout
+from keras.layers import Average, dot,Permute, Lambda,Layer, add, Concatenate, Dense, LSTM, Input, concatenate, merge, Add, Reshape, Flatten
+from keras.models import Model, load_model
+from keras.models import Sequential
+from numpy import newaxis
+from keras.models import model_from_json
+from keras import callbacks
+from keras.callbacks import ModelCheckpoint
+from collections import defaultdict
+from scipy.stats.stats import pearsonr
+from keras import backend as K
+from sklearn.model_selection import StratifiedKFold
+
+K.os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+K.os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+
+# FUNCTON FOR MATRIX MULTIPLICATION
+class OuterProduct(Layer):
+    def __init__(self, name="OuterProduct", **kwargs):
+        super().__init__()
+
+    def build(self, input_shape):
+        self.length = input_shape[0][1]
+        super().build(input_shape)
+
+    def call(self, x):
+        a, b = x
+        #        print(a.shape,b.shape,a[:, newaxis, :].shape,b[:, :, newaxis].shape)
+        outerProduct = a[:, newaxis, :] * b[:, :, newaxis]
+        #       print(outerProduct.shape)
+        outerProduct = K.sum(outerProduct, axis=-1)
+        #      print(outerProduct.shape)
+
+        return outerProduct
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], input_shape[0][1], input_shape[0][1])
+
+
+# SUBSTITUTE WORD WITH INDEX
+def replace_matched_items(array, dictionary):
+    for lst in array:
+        for ind, item in enumerate(lst):
+            lst[ind] = dictionary.get(item, item)
+
+
+# CLEAN ZEROS
+def cleaningZero(array):
+    for elem in array:
+        while 0 in elem:
+            elem.remove(0)
+
+
+target = 25
+
+
+# SAVE ANSWER INDEXS
+def getAnswerIDX(pred):
+    x = {}
+    y = []
+    for i in range(0, len(pred)):
+        x[i] = float(pred[i])
+    sorted_x = sorted(x.items(), key=operator.itemgetter(1))
+    sorted_x.reverse()
+    for j in range(0, len(sorted_x)):
+        y.append(sorted_x[j][0])
+    return y[0:target]
+
+
+def getAnswer(resp, test):
+    lst = resp
+    ans = []
+    for i in lst:
+        ans.append(test[i])
+    return ans
+
+
+# SPLIT ANSWERS
+def printAnswer(a):
+    answers = []
+
+    for j in range(0, len(a)):
+        sentence = ""
+        for i in a[j]:
+            sentence = sentence + " " + i
+        answers.append(sentence)
+    return answers
+
+
+# LOAD TOKENIZER FILES
+def loadSentences(filename):
+    array = []
+    with open(filename, encoding='utf8') as f:
+        ar = []
+        for line in f:
+            if (str(line) != "[\n") & (str(line) != "]\n"):
+                ar.append(re.sub('\n$', '', line))
+                # print (line)
+            if (str(line) == "]\n"):
+                array.append(ar)
+                ar = []
+
+    f.close()
+    return array
+
+if __name__ == '__main__':
+
+    # ANSWERING TEST
+    original_A_test = loadSentences("/home/inversi/FAQProject/Armillotta/tokenizer/tokenizerAnswer.txt")
+    A_test = loadSentences("/home/inversi/FAQProject/Armillotta/tokenizer/tokenizerAnswer.txt")
+
+    # QUESTION TEST--> 1130 QUESTIONS
+    original_Q_test = loadSentences("/home/inversi/FAQProject/Armillotta/tokenizer/tokenizerQTest.txt")
+    Q_test = loadSentences("/home/inversi/FAQProject/Armillotta/tokenizer/tokenizerQTest.txt")
+
+    # CONV2D - MAXPOOLING MODEL
+    product = load_model("Q_A_Conv2D_MaxPool.h5",
+                         custom_objects={'OuterProduct': OuterProduct})
+
+    # LOAD GENSIM MODEL WITH WORD2VEC
+    model_W2C = gensim.models.Word2Vec.load('mymodelFull')
+
+    # vocab - uso lo stesso vocab
+    vocab = dict([(k, v.index) for k, v in model_W2C.wv.vocab.items()])
+
+    Respond = []
+    Ids = []
+
+    for i in range(0, len(original_Q_test)):
+        start_time = time.time()
+        Q_test = original_Q_test[i]
+        Q_test = [Q_test] * len(A_test)
+
+        # REPLACE A_TEST AND Q_TEST WITH VOCAB INDEX
+        replace_matched_items(A_test, vocab)
+        cleaningZero(A_test)
+        replace_matched_items(Q_test, vocab)
+        cleaningZero(Q_test)
+
+        # pad documents with original max len
+        max_len = max(len(max(A_test, key=len)), len(max(Q_test, key=len)))
+        Q_test_padded = keras.preprocessing.sequence.pad_sequences(Q_test, padding='post', maxlen=max_len)
+        A_test_padded = keras.preprocessing.sequence.pad_sequences(A_test, padding='post', maxlen=max_len)
+
+        for j in range(0, 1):
+
+            # CONV2D MAXPOOLING FOR QUESTION 1
+            prodQ1 = product.predict([Q_test_padded, A_test_padded])
+            ansID = getAnswerIDX(prodQ1)
+            AnswersConvQ1 = getAnswer(ansID, original_A_test)
+
+            # SAVE CONV2D MAXPOOLING ANSWERS IN A TXT FILE
+            Answers = printAnswer(AnswersConvQ1)
+
+            print(i + 1)
+            print("--- %s seconds ---" % (time.time() - start_time))
+
+            with open('outcome_final.csv', 'a', newline='', encoding="utf-8") as csvfile:
+                fieldnames = ['idx', 'risposte']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+
+                for i in range(len(ansID)):
+                    writer.writerow({'idx': ansID[i], 'risposte': Answers[i]})
---
+++ experiments/vector_50/conv_QQA/Conv2D-MaxPoolingQQA.py
@@ -0,0 +1,237 @@
+import os
+import sys
+import certifi
+import unicodedata
+import pandas as pd
+import numpy as np
+import re
+import csv
+import math
+import requests
+import time
+import random
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import gensim
+import keras
+import h5py
+import pydot
+import os
+import sklearn
+import operator
+
+from keras.layers.merge import Add
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical, plot_model
+from keras.layers import Conv2D, MaxPooling2D, Embedding, Merge, LSTM, Bidirectional, GlobalMaxPooling1D, Dot, Lambda, Dropout
+from keras.layers import Average, dot,Permute, Lambda,Layer, add, Concatenate, Dense, LSTM, Input, concatenate, merge, Add, Reshape, Flatten
+from keras.models import Model, load_model
+from keras.models import Sequential
+from numpy import newaxis
+from keras.models import model_from_json
+from keras import callbacks
+from keras.callbacks import ModelCheckpoint, EarlyStopping,CSVLogger
+from collections import defaultdict
+from scipy.stats.stats import pearsonr
+from keras import backend as K
+from keras import metrics
+from sklearn.model_selection import StratifiedKFold
+from keras import metrics
+
+#LOAD TOKENIZER FILES
+def loadSentences(filename):
+    array = []
+    with open(filename) as f:
+        ar = []
+        for line in f:
+            if (str(line) != "[\n") & (str(line) != "]\n"):
+                ar.append(re.sub('\n$', '', line))
+                #print (line)
+            if (str(line) == "]\n"):
+                array.append(ar)
+                ar = []
+            
+    f.close()
+    return array
+    
+#SUBSTITUTE WORD WITH INDEX
+def replace_matched_items(array, dictionary):
+       for lst in array:
+              for ind, item in enumerate(lst):
+                          lst[ind] = dictionary.get(item, item)       
+                          
+                          
+#CLEAN ZEROS
+def cleaningZero(array):
+       for elem in array:
+            while 0 in elem: 
+                elem.remove(0)
+
+
+if __name__ == '__main__':
+    
+    
+    K.os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+    K.os.environ["CUDA_VISIBLE_DEVICES"]="3"
+
+    #LOAD GENSIM MODEL WITH WORD2VEC
+    model_W2C = gensim.models.Word2Vec.load('Armillotta/model/vector_50/mymodelFull')
+
+    # load the whole embedding into memory
+    embeddings_index = dict()
+    f = open('Armillotta/wiki-w2v/it-vectors.50.5.50.w2v', encoding = "utf-8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    print('Loaded %s word vectors.' % len(embeddings_index))
+
+    # create a weight matrix for words in training docs
+    vocab_size = len(model_W2C.wv.vocab)
+    embedding_matrix = np.zeros((vocab_size, 50))
+    for i in range(len(model_W2C.wv.vocab)):
+        embedding_vector = embeddings_index.get(model_W2C.wv.index2word[i])
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    
+
+    #ANSWER TRAINING
+    original_QA_train = loadSentences("Armillotta/tokenizer/tokenizerQuestionAnswer.txt")
+    QA_train = loadSentences("Armillotta/tokenizer/tokenizerQuestionAnswer.txt")
+
+    #QUESTION  TRAINING
+    original_Q_train = loadSentences("Armillotta/tokenizer/tokenizerQuestion.txt")
+    Q_train = loadSentences("Armillotta/tokenizer/tokenizerQuestion.txt")
+
+    #DATASET MANIPULATION FOR TRAINING
+    #CREATE 2 TRAIN WITH CORRECT QUESTION-ANSWER AND INCORRECT QUESTION-ANSWER
+    new = []
+    #A_train raddoppiamo il train aggiungendo risposte casuali
+    for i in range(0,len(original_QA_train)):
+        new.append(random.choice(QA_train))
+    QA_train.extend(new)
+
+    new = []
+    #QA_train raddoppiamo il train aggiungendo risposte casuali
+    for i in range(0,len(original_Q_train)):
+        new.append(random.choice(Q_train))
+    Q_train.extend(new)
+
+    #Y_train LABEL CLASS FOR EVALUATION TRAINING
+    Y_train = []
+    for i in range(0, (int(len(QA_train)/2))):
+        a=[]
+        a.append(1)
+        Y_train.append(a)
+    for i in range(int(len(QA_train)/2+1), len(QA_train) + 1):
+        a=[]
+        a.append(0)
+        Y_train.append(a)
+    Y_train = np.array(Y_train)
+
+    
+                    
+                    
+    #vocab - uso lo stesso vocab
+    vocab = dict([(k, v.index) for k, v in model_W2C.wv.vocab.items()])
+
+    #REPLACE  WITH VOCAB INDEX
+    replace_matched_items(Q_train,vocab)
+    cleaningZero(Q_train)
+
+    replace_matched_items(QA_train,vocab)
+    cleaningZero(QA_train)
+
+
+    #PAD DOCUMENTS FOR QUESTION AND ANSWER
+    max_len = max(len(max(QA_train,key=len)), len(max(Q_train,key=len)))
+    Q_train_padded = keras.preprocessing.sequence.pad_sequences(Q_train, padding='post', maxlen = max_len)
+    QA_train_padded = keras.preprocessing.sequence.pad_sequences(QA_train, padding='post', maxlen = max_len)
+
+
+    #FUNCTON FOR MATRIX MULTIPLICATION
+    class OuterProduct(Layer):
+        def __init__(self,name="OuterProduct",**kwargs):
+            super().__init__()
+
+        def build(self,input_shape):
+            self.length = input_shape[0][1]
+            super().build(input_shape)
+
+        def call(self,x):
+            a, b = x
+    #        print(a.shape,b.shape,a[:, newaxis, :].shape,b[:, :, newaxis].shape)
+            outerProduct = a[:, newaxis, :] * b[:, :, newaxis]
+     #       print(outerProduct.shape)
+            outerProduct = K.sum(outerProduct,axis=-1)
+      #      print(outerProduct.shape)
+
+            return outerProduct
+
+        def compute_output_shape(self, input_shape):
+            return (input_shape[0][0], input_shape[0][1], input_shape[0][1])
+            
+            
+    #Save the model after every epoch. https://keras.io/callbacks/#modelcheckpoint
+    checkpoint = ModelCheckpoint("Armillotta/keras_models/Q_QA_Conv2D_MaxPool.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    #Stop training when a monitored quantity has stopped improving. https://keras.io/callbacks/#earlystopping
+    csv_logger = CSVLogger('Armillotta/history/CONV_Q-QA/log.csv', append=True, separator=';')
+    callbacks_list = [checkpoint,csv_logger]
+
+
+    #LAYER EMBEDDING-BiLSTM FOR ANSWER
+    QA_input = Input(batch_shape=(None,QA_train_padded.shape[1],), name='A_input')
+    QA = Embedding(len(vocab),output_dim=50, weights=[embedding_matrix],name="Embedding_A",trainable=True)(QA_input)
+    QA_LSTM = Bidirectional(LSTM(50, dropout=0.5, recurrent_dropout=0.5,return_sequences=True),name="LSTM_A")(QA)
+
+
+    #LAYER EMBEDDING-BiLSTM FOR QUESTIONS
+    Q_input = Input(batch_shape=(None,Q_train_padded.shape[1],),  name='Q_input')
+    Q = Embedding(len(vocab),output_dim=50,name="Embedding_Q",trainable=True)(Q_input)
+    Q_LSTM = Bidirectional(LSTM(50, dropout=0.5, recurrent_dropout=0.5,return_sequences=True),name="LSTM_QA")(Q)
+
+    #PRODOTTO DELLE MATRICI EMB
+    M1 = OuterProduct()([QA, Q])
+    M1 = Reshape((-1,QA_train_padded.shape[1],Q_train_padded.shape[1]),name="M1_Embedding")(M1)
+
+    M2 = OuterProduct()([QA_LSTM, Q_LSTM])
+    M2 = Reshape((-1,QA_train_padded.shape[1],Q_train_padded.shape[1]),name="M2_LSTM")(M2)
+
+    M12 = Concatenate(axis=1,name="M1xM2")([M1, M2])
+
+
+    #CONVUTIONAL
+    Convo = Conv2D(8, 3, activation='relu', data_format='channels_first',name="Convolutional")(M12)
+    Convo = Dropout(.5)(Convo)
+
+    #MAXPOOLING
+    Pool = MaxPooling2D(pool_size=(2, 2), data_format='channels_first',name="Max_Pooling")(Convo)
+    Pool = Dropout(.5)(Pool)
+    Pool = Flatten()(Pool)
+
+    #MLP
+    dense = Dense(200, activation='relu')(Reshape((-1,))(Pool))
+    dense = Dropout(.5)(dense)
+    dense= Dense(100, activation='relu')(dense)
+    dense = Dropout(.5)(dense)
+
+    output = Dense(1,activation='sigmoid')(dense)
+
+    model = Model(inputs=[Q_input,QA_input], outputs=output)
+    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy',metrics.mean_squared_error])
+
+    #model.summary(line_length=200)
+    print("Model Ready...")
+
+    #PRINT MODEL LAYERS ON FILE.PNG
+    plot_model(model, to_file='Armillotta/image/Q_QA_Conv2D_MaxPool.png', show_shapes=True, show_layer_names=True)
+
+
+    history = model.fit([Q_train_padded,QA_train_padded],Y_train, batch_size=25, epochs=200, validation_split=0.1,callbacks = callbacks_list)
+
+
+    
